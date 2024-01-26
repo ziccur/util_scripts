2960119086e0768dd0715d0e006ca9af413e844d
a <a>
2024-01-26 14:14:37 +0100

    create repository added

--- scripts/prgtest_install.sh
+++ scripts/prgtest_install.sh
@@ -19,6 +19,16 @@ read email
 git config --global user.name "$name"
 git config --global user.email "$email"
 
+firefox "https://32e23a8f-d5c1-46ef-bad7-31aa74cfb616-00-3dmsolo0u19xz.kirk.replit.dev/" &
+echo "Link SSH copiado: "
+read repository # Problems with name user, github, link
+
+#### CLONE REPOSITORY ####
+clear
+echo "##### Clonando repositorio en /home/$USER"
+cd ~
+git clone $repository
+
 
 
 
