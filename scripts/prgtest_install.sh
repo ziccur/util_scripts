4f33588d384ccf64659a1fd92647247fd1a77e53
Yeray <2023_yeray.reyes@iticbcn.cat>
2024-01-26 16:38:31 +0100

    auto clone repository

--- scripts/prgtest_install.sh
+++ scripts/prgtest_install.sh
@@ -5,9 +5,19 @@
 #                                                                                                                           #
 #############################################################################################################################
 
+#!/bin/bash
+
+#### TEST ZONE ####
+
 #### CONFIGURAR ENTORNO ####
 
+echo "A continuación pedirá la contraseña de usuario para instalar (Java & Git)"
 sudo apt-get install -y git > /dev/null
+sudo apt-get install -y default-jdk > /dev/null
+
+clear
+echo "Git instalado correctamente"
+echo "Java (default-jdk) instalado correctamente"
 
 #### CONFIGURACIÓN PERSONALIZADA DE GIT ####
 echo "Introduce tu nombre: "
@@ -19,15 +29,23 @@ read email
 git config --global user.name "$name"
 git config --global user.email "$email"
 
-firefox "https://32e23a8f-d5c1-46ef-bad7-31aa74cfb616-00-3dmsolo0u19xz.kirk.replit.dev/" &
-echo "Link SSH copiado: "
-read repository # Problems with name user, github, link
+echo "Nombre de usuario de Github"
+read ghUser
+
+#### REFUSED PART FOR USER COMODITY ####
+# firefox "https://32e23a8f-d5c1-46ef-bad7-31aa74cfb616-00-3dmsolo0u19xz.kirk.replit.dev/" &
+# echo "Link SSH copiado: "
+# read repository # Problems with name: user, github, link
 
 #### CLONE REPOSITORY ####
 clear
-echo "##### Clonando repositorio en /home/$USER"
+echo "##### Clonando repositorio en el Escritorio de $USER"
 cd ~
-git clone $repository
+git clone git@github.com:$ghUser/introprg.git
+clear
+
+echo "##### Repositorio clonado en /home/$USER/introprg #####"
+
 
 
 
