a54c669fa285fb8e9fdfcbaeccee02a2fd23a170
Sunrise <2023_alba.segura@iticbcn.cat>
2024-02-06 12:54:53 +0100

    test1

---
+++ EncriptPy/encript.py
@@ -0,0 +1,95 @@
+# Libraries
+from cryptography.fernet import Fernet
+import os
+
+# Initialize the list of files to encrypt
+fileList= []
+
+# Path of the directory to encrypt
+directory = ""
+
+# Files to ignore
+ignore_files = ['filekey.key', 'encript.py', 'decript.py']
+
+def key():
+    # Generate Key
+    key = Fernet.generate_key()
+    # Save the key in a file
+    with open('filekey.key', 'wb') as filekey:
+        filekey.write(key)
+def encript_files():
+    # opening the key
+    with open('filekey.key', 'rb') as filekey:
+        key = filekey.read()
+    
+    # using the generated key
+    fernet = Fernet(key)
+    
+    # opening the original file to encrypt
+    with open('nba.csv', 'rb') as file:
+        original = file.read()
+        
+    # encrypting the file
+    encrypted = fernet.encrypt(original)
+    
+    # opening the file in write mode and 
+    # writing the encrypted data
+    with open('nba.csv', 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+
+# Add files to the list
+def add_files_to_list(path):
+    for item in os.listdir(path):
+        #Ignore files
+        if item == ignore_files[0] or item == ignore_files[1]  or item == ignore_files[2]:
+            continue
+        else:
+            
+            #Store the files in the list 
+            full_path = os.path.join(path, item)
+            if os.path.isdir(full_path):
+                print("["+item + " Directory]")
+                #If they are in another directory, calls the function again
+                add_files_to_list(full_path)
+            else:
+                print("["+item + " File]")
+                #If is a file, append it to the list
+                fileList.append(full_path)
+
+def route():
+    #Ask for the directory
+    global directory
+    directory = input("Enter the directory path: ")
+    #Check if the directory exists
+    if os.path.exists(directory):
+        print("Directory exists")
+    else:
+        print("Directory does not exist")
+        route()
+
+def choose():
+    a = input("Encript [1] or Decript [2]: ")
+    if(a == "1"):
+        #Calling the function
+        route()
+        add_files_to_list(directory)
+        key()
+        print("Encript")
+    elif(a == "2"):
+        #Calling the function
+        add_files_to_list(directory)
+        print("Decript")
+    else:
+        print("Invalid option")
+        choose()
+
+
+choose()
+
+#Checks
+for i in fileList:
+   print(i)
+
+  #  for o in ignore_files:
+  #      print(o)
